15bf8c01ed7a43de853337cd5b70a19500d5346a
RoxanneLefebvre <102246244+RoxanneLefebvre@users.noreply.github.com>
2023-03-06 18:48:11 -0500

    roxanne - ajout de angular au projet(interface IBouteille, IlisteBouteille et affichage dun premier data)

---
+++ resources/frontend/angular2/src/app/bouteille/bouteille.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { IBouteille } from '../ibouteille';
+import { FetchService } from '../fetch.service';
+
+@Component({
+  selector: 'app-bouteille',
+  templateUrl: './bouteille.component.html',
+  styleUrls: ['./bouteille.component.sass']
+})
+export class BouteilleComponent implements OnInit{
+  // bouteille:IBouteille;
+  arrayBouteille:Array<IBouteille>;
+
+  constructor(public fetchSaq:FetchService){
+    this.arrayBouteille = [];
+
+  }
+
+ 
+
+  ngOnInit(): void {
+    this.fetchSaq.getBd().subscribe((response) => {
+      this.arrayBouteille = response.data;
+      
+      
+      
+    });
+  }
+}
