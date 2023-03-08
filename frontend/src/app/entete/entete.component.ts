fc41eb052e0b40eba918ff86ea4ced18d5c2e580
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-08 13:54:27 -0500

    acmb - ajout de la fonctionnalité logout dans le module entete

--- frontend/src/app/entete/entete.component.ts
+++ frontend/src/app/entete/entete.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+// import { TokenService } from './shared/token.service';
+import { TokenService } from '../shared/token.service';
+// import { AuthStateService } from './shared/auth-state.service';
+import { AuthStateService } from '../shared/auth-state.service';
+
+// import { AuthService } from '../shared/auth.service';
+
+// // User interface
+// export class User {
+//   name: any;
+//   email: any;
+// }
 
 @Component({
   selector: 'app-entete',
   templateUrl: './entete.component.html',
-  styleUrls: ['./entete.component.scss']
+  styleUrls: ['./entete.component.scss'],
 })
-export class EnteteComponent {
 
+export class EnteteComponent implements OnInit {
+  isSignedIn!: boolean;
+  title:string='FrontEnd';
+  constructor(
+    private auth: AuthStateService,
+    public router: Router,
+    public token: TokenService
+  ) {}
+  ngOnInit() {
+    this.auth.userAuthState.subscribe((val) => {
+      this.isSignedIn = val;
+    });
+  }
+  // Signout
+  signOut() {
+    this.auth.setAuthState(false);
+    this.token.removeToken();
+    this.router.navigate(['login']);
+  }
 }
