7936865ae0afe77f4880c06aa8ba7c58a4afcf65
colbertgendreau <97406341+colbertgendreau@users.noreply.github.com>
2023-03-08 11:24:56 -0500

    console log d'une variable d'envirenement dans app.component.ts pour tester

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from './shared/token.service';
 import { AuthStateService } from './shared/auth-state.service';
+import { environment } from './../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,7 +15,9 @@ export class AppComponent implements OnInit {
     private auth: AuthStateService,
     public router: Router,
     public token: TokenService
-  ) {}
+  ) {
+    console.log(environment.production);
+  }
   ngOnInit() {
     this.auth.userAuthState.subscribe((val) => {
       this.isSignedIn = val;
