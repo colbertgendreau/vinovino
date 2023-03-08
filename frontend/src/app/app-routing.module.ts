a89918e80a43a71233da130d933238391df9c6a7
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-08 13:43:33 -0500

    acmb - ajout des routes principales de l'application

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,14 +3,35 @@ import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './compte-utilisateur/signin/signin.component';
 import { SignupComponent } from './compte-utilisateur/signup/signup.component';
 import { UserProfileComponent } from './compte-utilisateur/user-profile/user-profile.component';
+
+import { EnteteComponent } from './entete/entete.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CellierComponent } from './cellier/cellier.component';
+import { ListeCellierComponent } from './liste-cellier/liste-cellier.component';
+import { AjoutBouteilleComponent } from './ajout-bouteille/ajout-bouteille.component';
+import { ModifBouteilleComponent } from './modif-bouteille/modif-bouteille.component';
+import { ModifCellierComponent } from './modif-cellier/modif-cellier.component';
+import { AjoutCellierComponent } from './ajout-cellier/ajout-cellier.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: SigninComponent },
   { path: 'register', component: SignupComponent },
   { path: 'profile', component: UserProfileComponent },
+  
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'liste-cellier', component: ListeCellierComponent },
+  { path: 'cellier', component: CellierComponent },
+  { path: 'ajouter-bouteille', component: AjoutBouteilleComponent },
+  { path: 'modifier-bouteille', component: ModifBouteilleComponent },
+  { path: 'ajouter-cellier', component: AjoutCellierComponent },
+  { path: 'modifier-cellier', component: ModifCellierComponent },
+
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
+
 export class AppRoutingModule {}
