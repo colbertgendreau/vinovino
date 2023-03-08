0093ecdfcc5d6ec6878d1facc0c71c71d9267ca2
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-08 10:38:52 -0500

    acmb - transfert du code du component bouteille au component ajout-bouteille

--- frontend/src/app/ajout-bouteille/ajout-bouteille.component.ts
+++ frontend/src/app/ajout-bouteille/ajout-bouteille.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IBouteille } from '../ibouteille';
+import { FetchService } from '../fetch.service';
 
 @Component({
   selector: 'app-ajout-bouteille',
   templateUrl: './ajout-bouteille.component.html',
   styleUrls: ['./ajout-bouteille.component.scss']
 })
-export class AjoutBouteilleComponent {
+export class AjoutBouteilleComponent implements OnInit{
+  arrayBouteille:Array<IBouteille>;
+  
+  constructor(public fetchSaq:FetchService){
+    this.arrayBouteille = [];
+    
+
+  }
+
+ 
+
+  ngOnInit(): void {
+    this.fetchSaq.getBouteilleSAQ().subscribe((response) => {
+      this.arrayBouteille = response.data;
+      
+      
+      
+    });
+  }
+
 
 }
