6c355621c23662aabda7a168fa2a15ffbf4e1c15
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-10 16:01:48 -0500

    acmb - suppression des conditions if pour traduire les messages d'erreur

--- frontend/src/app/compte-utilisateur/signup/signup.component.ts
+++ frontend/src/app/compte-utilisateur/signup/signup.component.ts
@@ -34,26 +34,6 @@ export class SignupComponent implements OnInit {
       (error) => {
         this.errors = error.error;
         this.errors = JSON.parse(this.errors);
-
-        if (this.errors.name[0] === "The name field is required.") {
-          this.errors.name = "Le champ nom est obligatoire.";
-        } else if (this.errors.name[0] === "The name must be between 2 and 100 characters.") {
-          this.errors.name = "Le nom doit comporter entre 2 et 100 caractères.";
-        }
-
-        if (this.errors.email[0] === "The email field is required.") {
-          this.errors.email = "Le champ courriel est obligatoire.";
-        } else if (this.errors.email[0] === "The email must be a valid email address.") {
-          this.errors.email = "Le courriel doit être valide.";
-        }
-
-        if (this.errors.password[0] === "The password field is required.") {
-          this.errors.password = "Le champ du mot de passe est obligatoire.";
-        } else if (this.errors.password[0] === "The password must be at least 6 characters.") {
-          this.errors.password = "Le mot de passe doit comporter au moins 6 caractères.";
-        } else if (this.errors.password[0] === "The password confirmation does not match.") {
-          this.errors.password = "La confirmation du mot de passe ne correspond pas.";          
-        }
       },
       () => {
         this.registerForm.reset();
