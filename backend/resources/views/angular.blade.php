9d287b8e6cccfc479cabaa09b93efe6692643903
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-03-10 09:29:03 -0500

    PCA - build

--- backend/resources/views/angular.blade.php
+++ backend/resources/views/angular.blade.php
@@ -4,9 +4,9 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<style>*{margin:0;font-family:Arial,Helvetica,sans-serif}:root{--primaire:#F80301;--secondaire:#FFB700;--secondaire-transparent80:rgba(255, 183, 0, .8);--white:#FFFFFF;--white-transparent80:rgba(255, 255, 255, .8);--gris-fonce:#444444;--black:#000000;--black-transparent40:rgba(0, 0, 0, .4);--black-transparent50:rgba(0, 0, 0, .5);--transparent100:rgba(0, 0, 0, 0);--smallest:2px;--smaller4:4px;--smaller:8px;--small:12px;--med:16px;--big:20px;--bigger:24px;--biggest:32px;--border-radius:50px;--border-radius15:15px;--border-radius5:5px;--bold:bold;--display-none:none}</style><link rel="stylesheet" href="/assets/angular/styles.7c3349999402225b.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/assets/angular/styles.7c3349999402225b.css"></noscript></head>
+<style>@font-face{font-family:ruda;src:url(Ruda-VariableFont_wght.af8f6ca7ee65c61b.ttf)}*{margin:0;font-family:ruda}:root{--primaire:#F80301;--secondaire:#FFB700;--secondaire-transparent80:rgba(255, 183, 0, .8);--white:#FFFFFF;--white-transparent80:rgba(255, 255, 255, .8);--gris-fonce:#444444;--black:#000000;--black-transparent40:rgba(0, 0, 0, .4);--black-transparent50:rgba(0, 0, 0, .5);--transparent100:rgba(0, 0, 0, 0);--smallest:2px;--smaller4:4px;--smaller:8px;--small:12px;--med:16px;--big:20px;--bigger:24px;--biggest:32px;--border-radius:50px;--border-radius15:15px;--border-radius5:5px;--bold:bold;--display-none:none}</style><link rel="stylesheet" href="/assets/angular/styles.eacf7178ea14fb6c.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/assets/angular/styles.eacf7178ea14fb6c.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="/assets/angular/runtime.89bfad0fe920d2c9.js" type="module"></script><script src="/assets/angular/polyfills.794d7387aea30963.js" type="module"></script><script src="/assets/angular/main.11a912a2ba0a08ba.js" type="module"></script>
+<script src="/assets/angular/runtime.89bfad0fe920d2c9.js" type="module"></script><script src="/assets/angular/polyfills.794d7387aea30963.js" type="module"></script><script src="/assets/angular/main.77837c697579cc8d.js" type="module"></script>
 
 </body></html>
