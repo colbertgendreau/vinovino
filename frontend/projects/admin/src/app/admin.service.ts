933d42490bc496431d803ce26a01201c1986e3ac
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-15 17:10:18 -0400

    acmb - ajout du service admin pour les fetch sur le serveur backend

---
+++ frontend/projects/admin/src/app/admin.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from "../environments/environment";
+import { IUser } from './iuser';
+import { IlisteUser } from './iliste-user';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AdminService {
+  // Important: Ne pas utilsier http://127.0.0.1:800, à la place utiliser ex:
+  // private urlBouteille:string = environment.apiUrl+"/api/bouteilles";
+
+  private urlUtilisateur:string = environment.apiUrl+"/api/utilisateurs";
+
+  constructor(private http:HttpClient) { }
+
+  getUtilisateur():Observable<IlisteUser> {
+    return this.http.get<IlisteUser>(this.urlUtilisateur);
+  }
+}
