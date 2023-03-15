200f599c4aad362ccbb6c91aed1af5aab2041ee8
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-15 17:15:27 -0400

    acmb - ajout du composant liste-catalogue

---
+++ frontend/projects/admin/src/app/liste-catalogue/liste-catalogue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-catalogue',
+  templateUrl: './liste-catalogue.component.html',
+  styleUrls: ['./liste-catalogue.component.scss']
+})
+export class ListeCatalogueComponent {
+
+}
