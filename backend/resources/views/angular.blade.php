137a63c5f09cc279156a5d9331da29b07f0c1369
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-03-14 21:00:55 -0400

    ÃÃPCA - Build

--- backend/resources/views/angular.blade.php
+++ backend/resources/views/angular.blade.php
@@ -5,9 +5,10 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="apple-touch-icon" href="../src/drunk.png">
 <style>@font-face{font-family:ruda;src:url(Ruda-VariableFont_wght.af8f6ca7ee65c61b.ttf)}*{margin:0;font-family:ruda}:root{--primaire:#F80301;--secondaire:#FFB700;--primaire-transparent80:#f80101bc;--secondaire-transparent80:rgba(255, 183, 0, .8);--secondaire-turquoise:#10D4E0;--secondaire-peche:#FFA156;--white:#FFFFFF;--white-transparent80:rgba(255, 255, 255, .8);--gris-fonce:#444444;--black:#000000;--black-transparent40:rgba(0, 0, 0, .4);--black-transparent50:rgba(0, 0, 0, .5);--transparent100:rgba(0, 0, 0, 0);--smallest:2px;--smaller4:4px;--smaller:8px;--small:12px;--med:16px;--big:20px;--bigger:24px;--biggest:32px;--border-radius:50px;--border-radius15:15px;--border-radius5:5px;--bold:bold;--display-none:none}</style><link rel="stylesheet" href="/assets/angular/styles.6e5ba23653412597.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="/assets/angular/styles.6e5ba23653412597.css"></noscript></head>
 <body>
   <app-root></app-root>
-<script src="/assets/angular/runtime.89bfad0fe920d2c9.js" type="module"></script><script src="/assets/angular/polyfills.794d7387aea30963.js" type="module"></script><script src="/assets/angular/main.f57d2aa1707a84ce.js" type="module"></script>
+<script src="/assets/angular/runtime.89bfad0fe920d2c9.js" type="module"></script><script src="/assets/angular/polyfills.794d7387aea30963.js" type="module"></script><script src="/assets/angular/main.44f757a9c0d26a84.js" type="module"></script>
 
 </body></html>
