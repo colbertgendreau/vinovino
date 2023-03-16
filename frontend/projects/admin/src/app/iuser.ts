76ba7093364550afe442761c7aa42b9756611993
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-16 18:04:37 -0400

    acmb - ajout de la colonne type et password obligatoire dans l'interface IUser

--- frontend/projects/admin/src/app/iuser.ts
+++ frontend/projects/admin/src/app/iuser.ts
@@ -2,7 +2,8 @@ export interface IUser {
     id : number;
     name : string;
     email : string;
-    password? : string;
+    type : string;
+    password : string;
     created_at? : string;
     updated_at? : string;
 }
