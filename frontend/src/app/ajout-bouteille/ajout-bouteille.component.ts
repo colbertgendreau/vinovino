97cb3aa080cd7f8f820f92a7eddc160655588195
Colbert <colbertgen@gmail.com>
2023-03-16 17:14:58 -0400

    colbert - scroll up apres choisir bouteille

--- frontend/src/app/ajout-bouteille/ajout-bouteille.component.ts
+++ frontend/src/app/ajout-bouteille/ajout-bouteille.component.ts
@@ -38,6 +38,10 @@ export class AjoutBouteilleComponent implements OnInit{
   isDataSelected:boolean;
 
   formSubmitted = false;
+  spin: boolean;
+  hide: boolean;
+  hideForm: boolean = false;
+
   
   
   constructor(
@@ -59,17 +63,37 @@ export class AjoutBouteilleComponent implements OnInit{
   }
 
   filterData(searchTerm: string) {
-    if (this.searchTerm === '') {
-      this.filteredData = [];
-    }else{
+    this.spin = true;
 
+      console.log("recherche commencé");
+      
+      
+      if (this.searchTerm === '') {
+          this.filteredData = [];
+        }else{
+            this.spin = true;
+            this.hide = true;
+            this.hideForm = true;
+            
       this.filteredData = this.arrayBouteille.filter(item =>
         item.nom.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
+    this.spin = false;
+    this.hide = false;
+
   }
 
   selectData(bouteille: any) {
+
+    window.scroll({ 
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+ });
+
+    this.hideForm = false;
+
     this.selectedData = bouteille;
     console.log(this.selectedData);
     
@@ -104,20 +128,6 @@ export class AjoutBouteilleComponent implements OnInit{
   onInputChange() {
     this.isDataSelected = false;
     
-    // const idControl = this.ajouterBouteilleForm.get('id');
-    // if (idControl) {
-    //   idControl.enable();
-    // }
-    
-    // const nomControl = this.ajouterBouteilleForm.get('nom');
-    // if (nomControl) {
-    //   nomControl.enable();
-    // }
-
-    // const controls = this.ajouterBouteilleForm.controls;
-    // Object.keys(controls).forEach(controlName =>
-    //   controls[controlName].enable()
-    // );
   }
 
   ajouter() {
