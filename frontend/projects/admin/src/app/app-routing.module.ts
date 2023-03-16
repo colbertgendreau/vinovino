9f586fdca782903388fca1c05738c4d87c9d71f6
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-16 18:02:13 -0400

    acmb - ajout de la route ajout-admin et changement de nom de la route admin

--- frontend/projects/admin/src/app/app-routing.module.ts
+++ frontend/projects/admin/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AjoutAdminComponent } from './ajout-admin/ajout-admin.component';
 import { ListeCatalogueComponent } from './liste-catalogue/liste-catalogue.component';
 import { ListeUsagerComponent } from './liste-usager/liste-usager.component';
 
 const routes: Routes = [
-  { path : "", component : ListeUsagerComponent },
+  { path : "admin", component : ListeUsagerComponent },
   { path : "catalogue", component : ListeCatalogueComponent },
+  { path : "ajout-admin", component : AjoutAdminComponent },
 ];
 
 @NgModule({
