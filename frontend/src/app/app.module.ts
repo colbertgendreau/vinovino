c805826480961ec547a15321474deccffba5a5bd
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-03-16 15:52:17 -0400

    -Nicolas simple renommage des components effacer-cellier

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ModifCellierComponent } from './modif-cellier/modif-cellier.component';
 import { AjoutCellierComponent } from './ajout-cellier/ajout-cellier.component';
 import { MesBouteillesComponent } from './mes-bouteilles/mes-bouteilles.component';
 import { ListeMesbouteillesComponent } from './liste-mesbouteilles/liste-mesbouteilles.component';
-import { EffacerModalComponent } from './effacer-modal/effacer-modal.component';
+import { EffacerModalComponent } from './effacer-cellier-modal/effacer-cellier-modal.component';
 // import { ServiceWorkerModule } from '@angular/service-worker';
 
 
