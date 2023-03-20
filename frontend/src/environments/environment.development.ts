684d52f4fba2601f801302060bfe294c4379ddb1
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-03-20 09:59:59 -0400

    PCA - Fix: ajout de profilPrefix: 'profil/' dans environment.developpement.ts et environment.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000'
+  apiUrl: 'http://127.0.0.1:8000',
+  profilPrefix: 'profil/',
 };
+//quand on utilise ng serve avec le port 4200
