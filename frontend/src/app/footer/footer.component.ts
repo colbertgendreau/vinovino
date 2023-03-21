cdc5e9c7794d8f781394796b0aae1abea6846aae
Colbert <colbertgen@gmail.com>
2023-03-21 15:21:45 -0400

    colbert - styling et logique du footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -8,108 +8,114 @@ import { AjoutCellierComponent } from '../ajout-cellier/ajout-cellier.component'
 import { ModifCellierComponent } from '../modif-cellier/modif-cellier.component';
 import { ModifBouteilleComponent } from '../modif-bouteille/modif-bouteille.component';
 
-
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+  @Input() id!: number;
 
-    @Input() id!: number;
+  classeActuelle: object;
+  pageActuelle: string;
+  idCellier: number;
+  btnAjouter: string;
 
+  constructor(
+    public router: Router,
+    private route: ActivatedRoute
+  ) 
+  {}
 
-    pageActuelle:object;
-    idCellier:number;
+  ngOnInit() {
+    this.classeActuelle = this.route.component;
+    console.log(this.classeActuelle);
 
-    
-    
-    constructor(
-        public router: Router,
-        private route: ActivatedRoute,
-        // private boutonAjouterBouteille: AjoutBouteilleComponent,
-    ) {}
+    this.route.params.subscribe((params) => {
+      this.idCellier = params['id'];
+    });
 
-    
-    
-    ngOnInit() {
-        this.pageActuelle = this.route.component;
-        console.log(this.pageActuelle);
-        
-        this.route.params.subscribe((params) => {
-            this.idCellier = params['id'];
-            console.log(this.idCellier);          
-        });
+    switch (this.classeActuelle) {
+      case CellierComponent:
+        this.pageActuelle = 'cellier';
+        break;
+      case ListeCellierComponent:
+        this.pageActuelle = 'listeCellier';
+        break;
+      case AjoutBouteilleComponent:
+        this.pageActuelle = 'ajoutBouteille';
+        break;
+      case AjoutCellierComponent:
+        this.pageActuelle = 'ajoutCellier';
+        break;
+      case ModifCellierComponent:
+        this.pageActuelle = 'cellier';
+        break;
+      case ModifBouteilleComponent:
+        this.pageActuelle = 'modifBouteille';
+        break;
+    default:
+        this.pageActuelle = 'autrePage';
+        break;
+    }
+  }
 
+  back() {
+    console.log(this.pageActuelle);
+    if (this.pageActuelle == 'cellier') {
+      this.router.navigate(['liste-cellier']);
+    } else if (this.pageActuelle == 'listeCellier') {
+      console.log('doit etre grisé');
+    } else if (this.pageActuelle == 'ajoutBouteille') {
+      this.router.navigate(['/cellier/' + this.idCellier]);
+    } else if (this.pageActuelle == 'ajoutCellier') {
+      this.router.navigate(['liste-cellier']);
+    } else if (this.pageActuelle == 'modifCellier') {
+      this.router.navigate(['liste-cellier']);
+    } else if (this.pageActuelle == 'modifBouteille') {
+      this.router.navigate(['/cellier/' + this.id]);
+
+      // this.router.navigate(['liste-cellier']);
     }
 
-    
-    
-    back() {
-              
-  
-        console.log(this.pageActuelle);
-        if(this.pageActuelle == CellierComponent) {
-            this.router.navigate(['liste-cellier']);
-        } else if(this.pageActuelle == ListeCellierComponent) {
-            console.log('doit etre grisé');
-        } else if(this.pageActuelle == AjoutBouteilleComponent) {
-            this.router.navigate(['/cellier/'+this.idCellier]);
-        } else if(this.pageActuelle == AjoutCellierComponent) {
-            this.router.navigate(['liste-cellier']);   
-        } else if(this.pageActuelle == ModifCellierComponent) {
-            this.router.navigate(['liste-cellier']);   
-        } else if(this.pageActuelle == ModifBouteilleComponent) {
-            this.router.navigate(['/cellier/'+this.id]);
-            
-
-            // this.router.navigate(['liste-cellier']);   
-        }
-        
-        window.scroll({ // pour scroll up 
-        top: 0, 
-        left: 0, 
-        behavior: 'smooth' 
-        });
+    window.scroll({
+      // pour scroll up
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 
   ajouter() {
     console.log('ajouter bouteille');
     console.log(this.pageActuelle);
-    if(this.pageActuelle == CellierComponent) {
-        console.log('ajout bouteille');
-        this.router.navigate(['/ajouter-bouteille/'+this.idCellier]);
-    } else if(this.pageActuelle == ListeCellierComponent) {
-        console.log('ajout cellier');
-        this.router.navigate(['ajouter-cellier']);
+    if (this.pageActuelle == 'cellier') {
+      this.btnAjouter = 'ajoutCellier';
+      this.router.navigate(['/ajouter-bouteille/' + this.idCellier]);
+    } else if (this.pageActuelle == 'listeCellier') {
+      this.btnAjouter = 'ajoutBouteille';
+      this.router.navigate(['ajouter-cellier']);
+    } else {
+        console.log("different bouton ajouter");
         
-    } else if(this.pageActuelle == AjoutBouteilleComponent) {
-        this.router.navigate(['/cellier/'+this.idCellier]);
-    } else if(this.pageActuelle == AjoutCellierComponent) {
-        this.router.navigate(['liste-cellier']);   
-    } else if(this.pageActuelle == ModifCellierComponent) {
-        this.router.navigate(['liste-cellier']);   
-    } else if(this.pageActuelle == ModifBouteilleComponent) {
-        this.router.navigate(['liste-cellier']);   
     }
 
-    window.scroll({ // pour scroll up 
-    top: 0, 
-    left: 0, 
-    behavior: 'smooth' 
+    window.scroll({
+      // pour scroll up
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
     });
-
   }
 
   goUp() {
-    console.log("par en haut");
-    
-    window.scroll({ // pour scroll up quand on arrive sur la page
-        top: 0, 
-        left: 0, 
-        behavior: 'smooth' 
-    });
+    console.log('par en haut');
 
+    window.scroll({
+      // pour scroll up quand on arrive sur la page
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
 }
