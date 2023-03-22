17cd26a6ae8ad9eecfa279bee3d46a894c87c11f
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-03-22 15:21:24 -0400

    acmb - suppression fonctions inutilisées app.component.ts

--- frontend/projects/admin/src/app/app.component.ts
+++ frontend/projects/admin/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, isDevMode } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/shared/token.service';
 import { AuthStateService } from 'src/app/shared/auth-state.service';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -27,27 +26,16 @@ export class AppComponent implements OnInit {
     } else {
       console.log('Production!');
     }
-    // console.log(this.auth.userAuthState);
     this.auth.userAuthState.subscribe((val) => {
       this.isSignedIn = val;
       console.log(this.isSignedIn);
       this.isOpen = !this.isOpen;
-      // console.log(this.isOpen);
     });
   }
-
-  // pageListeUsager() {
-  //   this.router.navigate(['liste-usager']);
-  // }
-
-  // pageCatalogue() {
-  //   this.router.navigate(['catalogue']);
-  // }
 
   signOut() {
     this.auth.setAuthState(false);
     this.token.removeToken();
     this.router.navigate(['admin']);
   }
-
 }
