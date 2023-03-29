8163e2f706554080132a9e1364e820da8b24e304
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-03-29 10:57:07 -0400

    Nicolas - fetch initial des infos dans notes

--- frontend/src/app/notes/notes.component.ts
+++ frontend/src/app/notes/notes.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Imesbouteilles } from '../imesbouteilles';
+import { ActivatedRoute } from '@angular/router';
+import { FetchService } from '../fetch.service';
+
+
 
 @Component({
   selector: 'app-notes',
@@ -10,16 +15,43 @@ export class NotesComponent implements OnInit {
   lesEtoiles = [1, 2, 3, 4, 5];
   noteSelectionnee: number = 0;
 
-  constructor() { }
+  
+  @Input() uneBouteille: Imesbouteilles;
+
+  
+  constructor(
+    private route: ActivatedRoute,
+    public fetchService: FetchService,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params);
+      
+      this.fetchService.showDetail(params['id']).subscribe((data: any) => {
+        this.uneBouteille = data.data;
+        this.noteSelectionnee = this.uneBouteille.notes;
+        
+        console.log(this.noteSelectionnee);
+        
+        console.log(this.uneBouteille);
+      });
+
+      // const id = params['id'];
+      // if (id) {
+      //   this.fetchBottleData(id);
+      // }
+    });
     
+    
+
   }
 
   RecupererNote(note: number) {
+    console.log(this.uneBouteille);
     this.noteSelectionnee = note;
     console.log('Note sélectionnée : ' + this.noteSelectionnee);
-    // you can add additional logic here to handle the selected star rating
+    
   }
 
 }
