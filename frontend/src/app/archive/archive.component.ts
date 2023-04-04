2dd1ad10a78e8ec6dffec62a4f850b7499fe2438
Annie-Claude Martin-Brochu <102246318+anniecmb@users.noreply.github.com>
2023-04-04 13:50:38 -0400

    acmb - fonctions commentées composant archive

--- frontend/src/app/archive/archive.component.ts
+++ frontend/src/app/archive/archive.component.ts
@@ -7,14 +7,10 @@ import { FetchService } from '../fetch.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Ibouteillecellier } from '../ibouteille-cellier';
 import { Imesbouteilles } from '../imesbouteilles';
-import { EffacerBouteilleModalComponent } from '../effacer-bouteille-modal/effacer-bouteille-modal.component';
 import { environment } from '../../environments/environment';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
-
-
-
-// User interface
+// interface User
 export class User {
   name: any;
   email: any;
@@ -33,23 +29,30 @@ export class ArchiveComponent implements OnInit {
   bouteillesArchivees: Array<Ibouteillecellier>;
   bouteille: Imesbouteilles;
   cellierId: number;
-  cellierNom:string;
+  cellierNom: string;
   isSignedIn!: boolean;
   UserProfile!: User;
   unCellier: any;
   spin: boolean = true;
   hide: boolean = true;
   pageCellier: boolean = true;
-
   counterValue: number = 0;
   quantite: number;
   id: number;
   celliers_nom: string;
-
   isVisible = false;
-
   imgBouteilleNonDisponible = environment.baseImg + 'img/nonDispo.webp';
 
+  /**
+   * Constructeur de la classe ArchiveComponent
+   * @param auth composant AuthStateService
+   * @param router composant Router
+   * @param token composant TokenService
+   * @param authService composant AuthService
+   * @param fetchService composant FetchService
+   * @param route composant Route
+   * @param snackBar composantMatSnackBar
+   */
   constructor(
     private auth: AuthStateService,
     public router: Router,
@@ -61,87 +64,69 @@ export class ArchiveComponent implements OnInit {
   ) {
     this.authService.profileUser().subscribe((data: any) => {
       this.UserProfile = data;
-      console.log(this.UserProfile);
     });
     this.bouteilles = [];
     this.bouteillesArchivees = [];
   }
 
+  /**
+   * Fonction initiale dès l'instanciation de la classe
+   */
   ngOnInit() {
-
     window.scroll({ // pour scroll up quand on arrive sur la page
       top: 0,
       left: 0,
       behavior: 'smooth'
     });
-
     this.auth.userAuthState.subscribe((val) => {
       this.isSignedIn = val;
-      console.log(this.isSignedIn);
     });
-
-      this.fetchService.getMesBouteilles().subscribe((data: any) => {
-          this.bouteilles = data.data;
-
-          for (var i = 0; i < this.bouteilles.length; i++) {
-            if (this.bouteilles[i].quantite === 0) {
-              this.bouteillesArchivees.push(this.bouteilles[i]);
-            }
-          }
-
-          console.log(this.bouteillesArchivees);
-
-          this.bouteillesArchivees.forEach(uneBouteille => {
-            this.cellierId = uneBouteille.celliers_id;
-          });
-          
-          console.log('les bouteilles du cellier');
-          console.log(this.bouteillesArchivees);
-          this.spin = false;
-          this.hide = false;
-          
-        });
+    this.fetchService.getMesBouteilles().subscribe((data: any) => {
+      this.bouteilles = data.data;
+      for (var i = 0; i < this.bouteilles.length; i++) {
+        if (this.bouteilles[i].quantite === 0) {
+          this.bouteillesArchivees.push(this.bouteilles[i]);
+        }
+      }
+      this.bouteillesArchivees.forEach(uneBouteille => {
+        this.cellierId = uneBouteille.celliers_id;
+      });
+      this.spin = false;
+      this.hide = false;
+    });
   }
 
-
+  /**
+   * Fonction qui modifie la quantité d'une bouteille dans les bouteilles archivées
+   * @param id nombre - L'id de la bouteille archivée
+   * @param quantite nombre - La quantité de la bouteille archivée (quantité à 0)
+   * @param celliers_nom chaîne - Le nom du cellier de la bouteille archivée
+   */
   modifier(id: number, quantite: number, celliers_nom: string) {
-
-    console.log(id + "id");
-    console.log(quantite + "quantite");
-    console.log(celliers_nom + "cellier_nom");
-
     this.fetchService.showBouteille(id).subscribe((data: any) => {
       this.bouteille = data.data;
       this.bouteille.quantite = quantite;
-
-      console.log(this.bouteille.celliers_id);
-      this.openSnackBar('La bouteille a été déplacée dans le cellier '+celliers_nom, 'Fermer');
-
+      this.openSnackBar('La bouteille a été déplacée dans le cellier ' + celliers_nom, 'Fermer');
       let updateBouteille: Imesbouteilles = this.bouteille;
-      // console.log(updateBouteille);
-      
       this.fetchService.modifBouteille(id, updateBouteille).subscribe((retour) => {
-  
         this.bouteillesArchivees.forEach(uneBouteille => {
           this.cellierId = uneBouteille.celliers_id;
-          // console.log(uneBouteille.celliers_id);
         });
-
-        this.bouteillesArchivees.forEach(uneBouteilleArchivee => {
-          if (uneBouteilleArchivee.quantite === 0) {
-            this.bouteillesArchivees.pop();
-          }
+        this.route.params.subscribe((params) => {
+          this.fetchService.getMesBouteilles().subscribe((data: any) => {
+            this.bouteillesArchivees = (data.data).filter(bouteille => bouteille.quantite === 0);
+          });
         });
-  
         this.spin = false;
-        this.hide = false;            
+        this.hide = false;
       });
     });
   }
 
-
-  // modal d'effacement
-
+  /**
+   * Fonction qui permet l'ouverture du modal pour supprimer une bouteille dans un cellier
+   * @param id nombre - L'id de la bouteille
+   */
   openModal(id: number) {
     console.log(id);
     console.log(this.isVisible);
@@ -149,17 +134,17 @@ export class ArchiveComponent implements OnInit {
     this.isVisible = true;
   }
 
-  closeModal() {
-    this.isVisible = false;
-  }
-
+  /**
+   * Fonction qui permet la fermeture du modal de suppression
+   */
   onModalClosed() {
     this.isVisible = false;
   }
 
+  /**
+   * Fonction qui permet de remonter en haut de la page lorsqu'on arrive sur la page
+   */
   goUp() {
-    console.log("par en haut");
-
     window.scroll({ // pour scroll up quand on arrive sur la page
       top: 0,
       left: 0,
@@ -167,19 +152,10 @@ export class ArchiveComponent implements OnInit {
     });
   }
 
-  pageCelliers() {
-    window.scroll({ // pour scroll up
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-    this.router.navigateByUrl('profil/liste-cellier');
-  }
-
   /**
-  * Cette fonction affiche un message de type snackbar.
-  * @param message Le message à afficher.
-  * @param action L'action à afficher sur le bouton de fermeture du snackbar.
+  * Fonction qui affiche un message de type snackbar
+  * @param message Le message à afficher
+  * @param action L'action à afficher sur le bouton de fermeture du snackbar
   */
   openSnackBar(message: string, action: string) {
     const config = new MatSnackBarConfig();
