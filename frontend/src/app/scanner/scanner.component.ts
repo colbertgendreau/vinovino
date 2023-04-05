f5b05f287d111f4a2517a6caa36573aef66cc7f9
Pedro Contreras Avendano <45802239+pcavendano@users.noreply.github.com>
2023-04-05 09:14:20 -0400

    Update scanner.component.ts

--- frontend/src/app/scanner/scanner.component.ts
+++ frontend/src/app/scanner/scanner.component.ts
@@ -57,7 +57,7 @@ export class ScannerComponent implements OnDestroy {
                   console.log('aqui');
                   devices.forEach((device) => {
                     alert('device2 - ' + JSON.stringify(device));
-                    if ( device.kind === 'videoinput' && device.label.match(/ack/) != null ) {
+                    if ( device.kind === 'videoinput' && device.label.match(/arrière/) != null ) {
                       alert('Back2 found! - ' + device.label);
                       console.log('deviceId: ', device.deviceId);
                       this.backCameraList.push({'deviceLabel': device.label, 'deviceId': device.deviceId});
